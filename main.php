d43f1fca93440208d6cf6ad16b11adb6f850ad64
ytanakag1 <ytanakag1@gmail.com>
2020-10-22 16:17:37 +0900

    768*1024

--- main.php
+++ main.php
@@ -6,14 +6,15 @@
 
 class Folderslider{
   public $urlslider;
+  public $file_dir;
 
   public function get_images(){
     $wudir = wp_upload_dir(); //...
-    $file_dir = $wudir['basedir']."/slider3/*" ; //var/www/html/wp-content/uploads/slider3/'
+    $this->file_dir = $wudir['basedir']."/slider3/" ; //var/www/html/wp-content/uploads/slider3/'
     $this->urlslider = $wudir['baseurl']."/slider3/" ; //'https://ui.org/wp-content/uploads/slider3/
 
     $img_names=[];
-    $filelist = glob($file_dir);
+    $filelist = glob($this->file_dir.'*');
 
       foreach ($filelist as $values) {
         # '/'で分解した最後がファイル名｡225x300が含むファイル名のみ取得
